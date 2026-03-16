943e64af7aa4458d794827b79d61880a7bea7a90
ANUPARNAGANGULY2000 <anuparnaganguly@gmail.com>
2026-03-17 01:01:48 +0530

    Clean up test folder and remove unused files

---
+++ build/run_all_experiments.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./bin/flexnoc irregular ../test/pr.dot 0.05 0.25 0.02
+./bin/flexnoc irregular ../test/hybrid1.dot 0.05 0.25 0.02
+./bin/flexnoc irregular ../test/hybrid2.dot 0.05 0.25 0.02
